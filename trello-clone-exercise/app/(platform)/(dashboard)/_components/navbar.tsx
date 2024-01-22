a297ba7e11b7d3a12ed7d9dbc22e4f25042f0b4c
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-22 11:35:27 +0000

    side bar for desktop and bigger screens

--- trello-clone-exercise/app/(platform)/(dashboard)/_components/navbar.tsx
+++ trello-clone-exercise/app/(platform)/(dashboard)/_components/navbar.tsx
@@ -8,6 +8,7 @@ export const Navbar = () => {
   return(
     <nav className="fixed z-50 px-4 top-0 w-full h-14 border-b shadow-sm
     bg-white flex items-center">
+      
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
