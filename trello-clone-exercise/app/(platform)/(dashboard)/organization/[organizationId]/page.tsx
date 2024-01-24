8645baa346b229dd470f462d02c31537d29581ec
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-24 16:28:36 +0000

    added hover information tooltip to the boards, explaining why do you just have 5 boards

--- trello-clone-exercise/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello-clone-exercise/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,18 @@
+import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
+
+import { Separator } from "@/components/ui/separator";
+
 
 const OrganizatioIdPage = async () => {
 
   return(
     <div className="w-ful mb-20">
       <Info />
+      <Separator className="my-4"/>
+      <div className="px-2 md:px4">
+        <BoardList />
+      </div>
     </div>
   );
 
