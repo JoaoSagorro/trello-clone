5c55e6d58852c5834de8c17194ff3d9f1819bfdb
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-24 09:10:33 +0000

    added delete pending state

--- trello-clone-exercise/app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ trello-clone-exercise/app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { deleteBoard } from "@/actions/delete-board";
+import { FormDelete } from "./form-delete";
 
 
 interface BoardProps {
@@ -19,9 +20,7 @@ export const Board = ({
       <p>
         Board title: {title}
       </p>
-      <Button type="submit" variant="destructive" size="sm">
-        Delete
-      </Button>
+      <FormDelete />
     </form>
   );
 };
