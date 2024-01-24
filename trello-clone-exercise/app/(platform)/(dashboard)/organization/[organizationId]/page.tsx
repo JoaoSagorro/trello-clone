c9648b347d09bc70f2138044239ca11b643bd4e5
Joao Sagorro <joao.sagorro@gmail.com>
2024-01-24 14:54:46 +0000

    changed files

--- trello-clone-exercise/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello-clone-exercise/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,23 +1,13 @@
-import { Board } from "./board";
-import { Form } from "./form";
-
-import { db } from "@/lib/db";
+import { Info } from "./_components/info";
 
 const OrganizatioIdPage = async () => {
 
-  const boards = await db.board.findMany();
-
   return(
-    <div className="flex flex-col spacy-y-4">
-        <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id}/>
-        ))}
-
-      </div>
+    <div className="w-ful mb-20">
+      <Info />
     </div>
   );
+
 };
 
 export default OrganizatioIdPage;
