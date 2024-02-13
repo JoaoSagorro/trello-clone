a7168bb92a48b25203f6b393b1bf34fcbec220bc
Joao Sagorro <joao.sagorro@gmail.com>
2024-02-13 19:45:08 +0000

    enabled editing of the lists

--- trello-clone-exercise/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ trello-clone-exercise/app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { useState, useRef, ElementRef } from "react";
 import { List } from "@prisma/client";
+import { useEventListener } from "usehooks-ts";
 
 interface ListHeaderProps {
   data: List;
@@ -9,11 +11,45 @@ interface ListHeaderProps {
 export const ListHeader = ({
   data,
 }: ListHeaderProps) => {
+
+  const [ title, setTitle ] = useState(data.title);
+  const [ isEditing, setIsEditing ] = useState(false);
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      formRef.current?.requestSubmit();
+    };
+  };
+
+  useEventListener("keydown", onKeyDown);
+
   return(
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
-      <div className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent">
-        {data.title}
+      {isEditing ? (
+        <p>Form</p>
+      ) : (
+      <div
+        onClick={enableEditing}
+        className="w-full text-sm px-2.5 py-1 h-7 font-medium border-transparent"
+      >
+        {title}
       </div>
+      )}
     </div>
   );
 };
